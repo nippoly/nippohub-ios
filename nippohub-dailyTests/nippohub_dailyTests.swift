264eecfbcd6af00c57b6c97eb81fff74dd1fed07
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-22 07:18:36 +0900

    first commit

---
+++ nippohub-dailyTests/nippohub_dailyTests.swift
@@ -0,0 +1,34 @@
+//
+//  nippohub_dailyTests.swift
+//  nippohub-dailyTests
+//
+//  Created by うさきち on 2019/03/22.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import XCTest
+@testable import nippohub_daily
+
+class nippohub_dailyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
