6a7f94d71e9c52fb3c8cb79cdd543d8ce66fdcfa
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-27 07:39:30 +0900

    add: 日報リストアイテム

---
+++ nippohub-daily/components/controllers/DailyReportListItem.swift
@@ -0,0 +1,13 @@
+//
+//  DailyReportListItem.swift
+//  nippohub-daily
+//
+//  Created by うさきち on 2019/03/27.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class DailyReportListItem: UITableViewCell {
+    @IBOutlet var labelTitle: UILabel!
+}
