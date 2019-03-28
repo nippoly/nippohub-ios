4f654e1549f14f7e71e4dfd9d00a7e453b3011a2
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-03-29 07:50:28 +0900

    add: 日報を渡して詳細ページに遷移

---
+++ nippohub-daily/view controllers/DailyReportShowViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DailyReportShowViewController.swift
+//  nippohub-daily
+//
+//  Created by うさきち on 2019/03/29.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class DailyReportShowViewController: UIViewController {
+    var dailyReport: DailyReport?
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        
+        print(dailyReport)
+    }
+}
