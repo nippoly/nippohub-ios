dcc9a0d7daa1e1386d986c9918fc82d14bca808d
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-09 07:39:52 +0900

    add: AccountManager追加

---
+++ nippohub-daily/Utilities/AccountManager.swift
@@ -0,0 +1,35 @@
+//
+//  AccountManager.swift
+//  nippohub-daily
+//
+//  Created by うさきち on 2019/04/09.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AccountManager {
+    static let manager = AccountManager()
+    
+    private let auth = Auth.auth()
+    
+    private init() {
+    }
+    
+    func didSetUp(listener: @escaping AuthStateDidChangeListenerBlock) {
+        auth.addStateDidChangeListener(listener)
+    }
+    
+    func signIn(email: String, password: String, completion: @escaping AuthDataResultCallback) {
+        auth.signIn(withEmail: email, password: password, completion: completion)
+    }
+    
+    func signUp(email: String, password: String, completion: @escaping AuthDataResultCallback) {
+        auth.createUser(withEmail: email, password: password, completion: completion)
+    }
+    
+    func currentUser() -> User? {
+        return auth.currentUser
+    }
+}
