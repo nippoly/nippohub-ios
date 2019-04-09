f4fe1b3f364339f96e6893cb2e1068914cf60406
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-10 07:37:47 +0900

    update: 認証済みの場合日報一覧にリダイレクト

---
+++ nippohub-daily/ViewControllers/LaunchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchViewController.swift
+//  nippohub-daily
+//
+//  Created by うさきち on 2019/04/09.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AccountManager.manager.didSetUp { [unowned self] auth, user in
+            if user != nil {
+                self.performSegue(withIdentifier: "launchToDailyReportIndexSegue", sender: nil)
+            } else {
+                self.performSegue(withIdentifier: "launchToSignInSegue", sender: nil)
+            }
+        }
+    }
+}
