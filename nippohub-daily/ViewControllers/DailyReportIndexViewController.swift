bb9093973b648e035b6760017cdd4a9b0bdf30e7
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-11 07:54:16 +0900

    fix: SIGBART

--- nippohub-daily/ViewControllers/DailyReportIndexViewController.swift
+++ nippohub-daily/ViewControllers/DailyReportIndexViewController.swift
@@ -24,16 +24,16 @@ class DailyReportIndexViewController: UIViewController {
         
         if let user = currentUser {
             let ref: DatabaseReference! = Database.database().reference()
-            var dailyReports: [DailyReport] = []
-            
-            ref.child("users/\(user.uid)/daily_reports").observe(.childAdded, with: { snapshot -> Void in
-                // TODO: 変換失敗時の処理を考える
-                print(snapshot.childrenCount)
-                let date = (try? DateConverter.converter.toDate(from: snapshot.childSnapshot(forPath: "date").value as! String)) ?? Date()
+
+            ref.child("users/\(user.uid)/daily_reports").observe(.value, with: { snapshot -> Void in
+                let dailyReports = snapshot.children.map({ dailyReportRaw -> DailyReport in
+                    let dailyReport = dailyReportRaw as! DataSnapshot
+                    // TODO: 変換失敗時の処理を考える
+                    let date = (try? DateConverter.converter.toDate(from: dailyReport.childSnapshot(forPath: "date").value as! String)) ?? Date()
+
+                    return DailyReport(id: dailyReport.key, date: date, title: dailyReport.childSnapshot(forPath: "title").value as! String, content: dailyReport.childSnapshot(forPath: "content").value as! String)
+                })
                 
-                dailyReports.append(DailyReport(id: snapshot.key, date: date, title: snapshot.childSnapshot(forPath: "title").value as! String, content: snapshot.childSnapshot(forPath: "content").value as! String))
-                
-                // TODO: ループ毎ではなくまとめて処理できるようにする
                 self.dailyReports = dailyReports
                 self.tableDailyReports.reloadData()
             })
