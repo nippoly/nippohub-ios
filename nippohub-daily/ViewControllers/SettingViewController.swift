721a9cb07ebb65d92a46f771f694a098aa9eaccf
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-04-27 09:15:26 +0900

    add: 設定ページ動作

---
+++ nippohub-daily/ViewControllers/SettingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SettingViewController.swift
+//  nippohub-daily
+//
+//  Created by うさきち on 2019/04/27.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        let storyBoard = self.storyboard!
+        
+        if index == 0 {
+            let agreementsViewController = storyBoard.instantiateViewController(withIdentifier: "AgreementsViewController")
+            
+            self.present(agreementsViewController, animated: true, completion: nil)
+        } else if index == 1 {
+            let privacyViewController = storyBoard.instantiateViewController(withIdentifier: "PrivacyViewController")
+            
+            self.present(privacyViewController, animated: true, completion: nil)
+        }
+    }
+}
