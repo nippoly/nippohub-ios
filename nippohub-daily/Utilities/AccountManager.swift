f5ef570936a7e18ae27a85afd983b1428ea2ad6e
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-07-01 07:51:00 +0900

    update: 余計なものは返さない

--- nippohub-daily/Utilities/AccountManager.swift
+++ nippohub-daily/Utilities/AccountManager.swift
@@ -27,18 +27,23 @@ final class AccountManager {
         auth.addStateDidChangeListener(listener)
     }
     
-    func signIn(email: String, password: String, completion: @escaping AuthDataResultCallback) {
-        auth.signIn(withEmail: email, password: password, completion: completion)
+    // TODO: error型を返さないようにする
+    func signIn(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        auth.signIn(withEmail: email, password: password) { res, err in
+            completion(err)
+        }
     }
     
-    func signUp(email: String, password: String, completion: @escaping AuthDataResultCallback) {
-        auth.createUser(withEmail: email, password: password, completion: completion)
+    func signUp(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        auth.createUser(withEmail: email, password: password) { res, err in
+            completion(err)
+        }
     }
     
     @discardableResult
     func signOut() -> Bool {
         do {
-           try auth.signOut()
+            try auth.signOut()
             
             return true
         } catch {
