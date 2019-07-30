2e960335d4a575618f718c48305121bba679b89b
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-07-31 07:22:35 +0900

    clean: 不要なメソッド

--- nippohub-daily/ViewsControllers/DailyReportIndex/DailyReportsTableViewController.swift
+++ nippohub-daily/ViewsControllers/DailyReportIndex/DailyReportsTableViewController.swift
@@ -32,26 +32,6 @@ final class DailyReportsTableViewController: UITableViewController {
         }
     }
     
-    // 日報に更新があった時日報一覧を最新にする
-    func updateDailyReports(dailyReport: DailyReport) {
-        let index = dailyReports.firstIndex { $0.id == dailyReport.id }
-        
-        if let index = index {
-            dailyReports[index] = dailyReport
-            dailyReports.sort { $0.date >= $1.date }
-        } else {
-            let indexToInsert = dailyReports.firstIndex { $0.date <= dailyReport.date }
-            
-            if let indexToInsert = indexToInsert {
-                dailyReports.insert(dailyReport, at: indexToInsert)
-            } else {
-                dailyReports.append(dailyReport)
-            }
-        }
-        
-        tableView.reloadData()
-    }
-    
     static func instantiate() -> DailyReportIndexViewController {
         return UIStoryboard(name: "DailyReportIndex", bundle: nil).instantiateInitialViewController() as! DailyReportIndexViewController
     }
