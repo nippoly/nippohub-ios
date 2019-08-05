fcc8e66f8b9d01bbd4916d7136b7f6f3c72c9bce
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-08-06 07:45:35 +0900

    add: 共有URL View

---
+++ nippohub-daily/ViewsControllers/DailyReportShow/DailyReportShareURLView/DailyReportShareURLView.swift
@@ -0,0 +1,25 @@
+//
+//  DailyReportShareUrlView.swift
+//  nippohub-daily
+//
+//  Created by うさきち on 2019/08/06.
+//  Copyright © 2019 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class DailyReportShareURLView: UIView {
+    @IBOutlet private weak var textFieldShareURL: UITextField!
+    @IBOutlet private weak var buttonShareStop: UIButton!
+    var shareURL: String = "" {
+        didSet {
+            textFieldShareURL.text = shareURL
+        }
+    }
+    var onTapStopShare: (() -> ())?
+
+    @IBAction
+    func tapStopShare() {
+        onTapStopShare?.self()
+    }
+}
